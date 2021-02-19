4447806dfcf724f52c8282eec0701a2aed65db28
marga2002@yandex.ru <marga2002@yandex.ru>
2021-02-19 10:19:16 +0300

    Логика отправки почты

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace TestConsole
 {
@@ -6,7 +8,24 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var from = new MailAddress("mymail@yandex.ru", "Rita");
+            var to = new MailAddress("mymail@gmail.com");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "Заголовок";
+            message.Body = "Текст письма";
+
+            var client = new SmtpClient("smtp.yandex.ru");
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "UserName",
+                //SecurePassword = 
+                Password = "PAssword!"
+            };
+
+            client.Send(message);
         }
     }
 }
