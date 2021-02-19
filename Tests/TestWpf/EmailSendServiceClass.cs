da5c7ca24a6c8c79a2976e9721b7aa0c318a823b
marga2002@yandex.ru <marga2002@yandex.ru>
2021-02-19 12:51:50 +0300

    Отправка почты с помощью WPF (основная часть домашнего задания)

---
+++ Tests/TestWpf/EmailSendServiceClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace TestWpf
+{
+    public class EmailSendServiceClass
+    {
+        private SmtpClient client;
+        private MailMessage message;
+
+        public EmailSendServiceClass(SmtpClient client, MailMessage message)
+        {
+            this.client = client;
+            this.message = message;
+
+        }
+        public void SendMail()
+        {
+            try
+            {
+                client.Send(message);
+
+                SendEndWindow sew = new SendEndWindow();
+                sew.IsSendOK.Text="Почта успешно доставлена!";
+                sew.IsSendOK.Foreground = System.Windows.Media.Brushes.Green;
+                sew.ShowDialog();
+                
+            }
+            catch (SmtpException)
+            {
+                SendEndWindow sew = new SendEndWindow();
+                sew.IsSendOK.Text = "Ошибка авторизации";
+                sew.IsSendOK.Foreground = System.Windows.Media.Brushes.Red;
+                sew.ShowDialog();
+                
+            }
+            catch (TimeoutException)
+            {
+                SendEndWindow sew = new SendEndWindow();
+                sew.IsSendOK.Text = "Ошибка адреса сервера";
+                sew.IsSendOK.Foreground = System.Windows.Media.Brushes.Red;
+                sew.ShowDialog();
+                
+            }
+        }
+    }
+}
