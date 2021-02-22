0758bcaadd5a276b57545bb339e390fa3f41fdbe
marga2002@yandex.ru <marga2002@yandex.ru>
2021-02-22 13:20:58 +0300

    Начало работы

---
+++ MailSender/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MailSender
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Exit_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
