a049abab9fc2cb6313487c846fc9e3fdce27d634
marga2002@yandex.ru <marga2002@yandex.ru>
2021-02-26 15:19:44 +0300

    Модель представления

---
+++ Tests/TestWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MailSender.lib.ViewModels.Base;
+
+namespace TestWpf.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+        private string _Title = "Test111";
+        public string Title { get=> _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //}
+            set => Set(ref _Title,value);
+        }
+    }
+}
