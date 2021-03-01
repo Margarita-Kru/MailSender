2be5cecaf7b09f250d5aa357145f290e3cd5c1f8
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-01 18:50:08 +0300

    Новое отображение панели элементов

--- MailSender/Controls/ItemsPanel.xaml.cs
+++ MailSender/Controls/ItemsPanel.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MailSender.Controls
 {
@@ -11,8 +13,110 @@ namespace MailSender.Controls
                 typeof(string),
                 typeof(ItemsPanel),
                 new PropertyMetadata(default(string)));
-        public string Title { get=>(string)GetValue(TitleProperty);
-            set => SetValue(TitleProperty, value); }
+        #region AddNewItemCommand: ICommand - добавление нового элемента
+        private readonly DependencyProperty AddNewCommandProperty=
+            DependencyProperty.Register(
+                nameof(AddNewItemCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(ICommand)));
+
+        public ICommand AddNewItemCommand
+        {
+            get => (ICommand)GetValue(AddNewCommandProperty);
+            set => SetValue(AddNewCommandProperty, value);
+        }
+        #endregion
+        #region EditItemCommand: ICommand - редактирование элемента
+        private readonly DependencyProperty EditCommandProperty =
+            DependencyProperty.Register(
+                nameof(EditItemCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(ICommand)));
+
+        public ICommand EditItemCommand
+        {
+            get => (ICommand)GetValue(EditCommandProperty);
+            set => SetValue(EditCommandProperty, value);
+        }
+        #endregion
+        #region RemoveItemCommand:ICommand - удаление элемента
+        private readonly DependencyProperty RemoveCommandProperty =
+            DependencyProperty.Register(
+                nameof(RemoveItemCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(ICommand)));
+
+        public ICommand RemoveItemCommand
+        {
+            get => (ICommand)GetValue(RemoveCommandProperty);
+            set => SetValue(RemoveCommandProperty, value);
+        }
+        #endregion
+        #region ItemSource:IEnumerable - элементы панели
+        private readonly DependencyProperty ItemSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemSource),
+                typeof(IEnumerable),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(IEnumerable)));
+
+        public IEnumerable ItemSource
+        {
+            get => (IEnumerable)GetValue(ItemSourceProperty);
+            set => SetValue(ItemSourceProperty, value);
+        }
+        #endregion
+        #region ItemSource:object - выбранный элемент
+        private readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItem),
+                typeof(object),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(object)));
+
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+        #endregion
+        #region ItemTemplate:DataTemplate - шаблон элемента выпадающего списка
+        private readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(
+                nameof(ItemTemplate),
+                typeof(DataTemplate),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(DataTemplate)));
+
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+        #endregion
+        #region DisplayMemberPath:string - имя отображаемого свойства
+        private readonly DependencyProperty DisplayMemberPathProperty =
+            DependencyProperty.Register(
+                nameof(DisplayMemberPath),
+                typeof(string),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(string)));
+
+        public string DisplayMemberPath
+        {
+            get => (string)GetValue(DisplayMemberPathProperty);
+            set => SetValue(DisplayMemberPathProperty, value);
+        }
+        #endregion
+        public string Title 
+        { 
+            get=>(string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value); 
+        }
+
         public ItemsPanel() => InitializeComponent();
     }
 }
