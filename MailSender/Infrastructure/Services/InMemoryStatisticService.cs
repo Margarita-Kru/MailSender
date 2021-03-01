b1e0f4be912597f16bdac744afb986599f47e620
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-01 12:23:01 +0300

    Исправление InMemoryStatisticService

--- MailSender/Infrastructure/Services/InMemoryStatisticService.cs
+++ MailSender/Infrastructure/Services/InMemoryStatisticService.cs
@@ -2,26 +2,17 @@
 using MailSender.lib.Interfaces;
 using System;
 using System.Diagnostics;
-using System.Threading;
 
 namespace MailSender.Infrastructure.Services
 {
-    internal class InMemoryStatisticService : IStatistic
+    internal class InMemoryStatisticService :IStatistic
     {
         private int _SendedMailsCount;
-        public int SendedMailsCount { get=> _SendedMailsCount; }
-        public void MailSended() 
-        {
-            _SendedMailsCount++;
-        }
+        public int SendedMailsCount => _SendedMailsCount;
+        public void MailSended() => _SendedMailsCount++;
         public int SendersCount => TestData.Senders.Count;
         public int RecipientsCount => TestData.Recipients.Count;
         private readonly Stopwatch _StopwatchTimer = Stopwatch.StartNew();
         public TimeSpan UpTime => _StopwatchTimer.Elapsed;
-        private Timer _Timer;
-        public InMemoryStatisticService()
-        {
-
-        }
     }
 }
