35e49ade612ea81e12fffa66a8b81581f64e7cbf
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-04 21:43:09 +0300

    Валидация посредством генерации исключения

--- MailSender/Models/Recipient.cs
+++ MailSender/Models/Recipient.cs
@@ -1,10 +1,21 @@
 ﻿using MailSender.Models.Base;
+using System;
 
 namespace MailSender.Models
 {
     public class Recipient : Entity
     {
-        public string Name { get; set; }
+        public string _Name;
+            public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Не задано имя пользователя!");
+                _Name = value;
+            }
+        }
         public string Address { get; set; }
     }
 }
