bc7956967a55c1319d717c3e7f61aff9b5380f74
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-04 22:20:24 +0300

    Валидация данных с помощью интерфейса

--- MailSender/Models/Recipient.cs
+++ MailSender/Models/Recipient.cs
@@ -1,12 +1,16 @@
 ﻿using MailSender.Models.Base;
 using System;
+using System.ComponentModel;
 
 namespace MailSender.Models
 {
-    public class Recipient : Entity
+    public class Recipient : Entity, IDataErrorInfo
     {
         public string _Name;
-            public string Name
+
+        public string this[string columnName] => throw new NotImplementedException();
+
+        public string Name
         {
             get => _Name;
             set
@@ -17,5 +21,23 @@ namespace MailSender.Models
             }
         }
         public string Address { get; set; }
+
+        string IDataErrorInfo.Error => null;
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
+            {
+                switch(PropertyName)
+                {
+                    default: return null;
+                    case nameof(Name):
+                        var name = Name;
+                        if (name is null) return "Имя не может быть пустой строкой";
+                        if (name.Length<3) return "Длина имени должна быть больше 3 символов";
+                        if (name.Length> 20) return "Длина имени не может быть больше 20 символов";
+                        return null;
+                }
+            }
+        }
     }
 }
