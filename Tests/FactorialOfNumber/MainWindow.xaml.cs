2be2e8eacb9dc55c1ccffff1358bf902f7dd813d
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-07 10:27:32 +0300

    Разделение на потоки с помощью диспетчера

--- Tests/FactorialOfNumber/MainWindow.xaml.cs
+++ Tests/FactorialOfNumber/MainWindow.xaml.cs
@@ -7,29 +7,39 @@ namespace FactorialOfNumber
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-        static int Sum(int n)
+        private void Sum(int n)
         {
             int sum = 0;
             for(int i=0;i<=n;i++)
                 sum += i;
-            return sum;
+            var Sum_work_tread_id = Thread.CurrentThread.ManagedThreadId;
+            Sum_TextBox.Dispatcher.Invoke(()=>Sum_TextBox.Text = sum.ToString());
+            
+
         }
         static long Factorial(long x)
         {
             if (x == 0)
-            {
                 return 1;
-            }
             else
-            {
                 return x * Factorial(x - 1);
-            }
         }
         private void Decide_Click(object sender, RoutedEventArgs e)
         {
             int number = Convert.ToInt32(number_TextBlock.Text);
-            Factorial_TextBox.Text = (Factorial(number)).ToString();
-            Sum_TextBox.Text = (Sum(number)).ToString();
+            var sum_thread = new Thread(() => Sum(number));
+            var work_tread_id = Thread.CurrentThread.ManagedThreadId;
+            sum_thread.Start();
+            //var factorial_return_thread = new Thread(() => FactorialReturn(number));
+            //sum_thread.Start();
+        }
+        private void FactorialReturn(int number)
+        {
+            Sum_TextBox.Dispatcher.Invoke(() =>
+            {
+                var sum_work_tread_id = Thread.CurrentThread.ManagedThreadId;
+                return Sum_TextBox.Text = Factorial(number).ToString();
+            });
         }
     }
 }
