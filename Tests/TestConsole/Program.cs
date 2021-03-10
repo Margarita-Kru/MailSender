45eb8efd9f24f8722201ffa7fefe1ac36e0687ad
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-10 22:30:22 +0300

    Логика вычисления произведения матриц

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,31 +1,57 @@
 ﻿using System;
-using System.Net;
-using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace TestConsole
 {
     class Program
     {
+        const int n = 3;
+        static int [,] NewMatrix(bool f)
+        {
+            int[,] A = new int[n, n];
+            Random rand = new Random();
+
+            for (int j = 0; j < n; j++)
+                for (int i = 0; i < n; i++)
+                {
+                    if(f==false)
+                        A[i, j] = rand.Next(0, 10);
+                    else
+                        A[i, j] = 0;
+                }
+            return A;
+        }
+        static int[,] Multi(int [,] A, int[,] B, int [,] C)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    C[i,j] = 0;
+                    for (int k = 0; k < n; k++)
+                        C[i,j] += A[i,k] * B[k,j];
+                }
+            return C;
+        }
+        static void print (int [,] C)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write($"{C[j, i]}   ");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
-            var from = new MailAddress("mymail@yandex.ru", "Rita");
-            var to = new MailAddress("mymail@gmail.com");
+            int [,] A = NewMatrix(false);
+            int [,] B = NewMatrix(false);
+            int[,] C = NewMatrix(true);
 
-            var message = new MailMessage(from, to);
-            message.Subject = "Заголовок";
-            message.Body = "Текст письма";
-
-            var client = new SmtpClient("smtp.yandex.ru");
-            client.EnableSsl = true;
-
-            client.Credentials = new NetworkCredential
-            {
-                UserName = "UserName",
-                //SecurePassword = 
-                Password = "PAssword!"
-            };
-
-            client.Send(message);
+            Multi(A, B, C);
+            print(C);
+            Console.ReadKey();
         }
     }
 }
