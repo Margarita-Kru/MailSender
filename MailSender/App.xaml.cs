b1fe0ecb55f5f774531441d07b8140e91422d178
marga2002@yandex.ru <marga2002@yandex.ru>
2021-03-15 20:04:25 +0300

    Инициализация БД

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using MailSender.Data;
 using MailSender.Infrastructure.Services;
 using MailSender.Infrastructure.Services.InDatabase;
-using MailSender.Infrastructure.Services.InMemory;
 using MailSender.lib;
-using MailSender.lib.Entities;
 using MailSender.lib.Interfaces;
 using MailSender.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Windows;
 
 namespace MailSender
 {
@@ -25,9 +24,21 @@ namespace MailSender
             ;
         public static IServiceProvider Services => Hosting.Services;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            using (var scope = Services.CreateScope())
+            {
+                var initializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+                initializer.InitializeAsync().Wait();
+            }
+                base.OnStartup(e);
+        }
+
         private static void ConfigureServices(HostBuilderContext host, IServiceCollection servises)
         {
             servises.AddDbContext<MailSenderDb>(opt => opt.UseSqlServer(host.Configuration.GetConnectionString("SqlServer")));
+            servises.AddTransient<DbInitializer>();
+
             servises.AddSingleton<MainWindowViewModel>();
             servises.AddSingleton<StatisticViewModel>();
 
@@ -38,14 +49,13 @@ namespace MailSender
 
             servises.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
 
-            servises.AddSingleton<IStatistic,InMemoryStatisticService>();
+            servises.AddSingleton<IStatistic, InMemoryStatisticService>();
 
 #if DEBUG
             servises.AddSingleton<IMailService, DebugMailService>();
 #else
             servises.AddSingleton<IMailService, SmtpMailService>();
 #endif
-
         }
     }
 }
---
+++ MailSender/Data/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using MailSender.lib.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailSender.Data
+{
+    public class DbInitializer
+    {
+        private readonly MailSenderDb _db;
+        public DbInitializer(MailSenderDb db)
+        {
+            _db = db;
+        }
+        public async Task InitializeAsync()
+        {
+            await _db.Database.MigrateAsync().ConfigureAwait(false);
+            if (!await _db.Senders.AnyAsync())
+            {
+                await _db.Senders.AddRangeAsync(TestData.Senders);
+                await _db.SaveChangesAsync();
+            }
+            if (!await _db.Recipients.AnyAsync())
+            {
+                await _db.Recipients.AddRangeAsync(TestData.Recipients);
+                await _db.SaveChangesAsync();
+            }
+            if (!await _db.Servers.AnyAsync())
+            {
+                await _db.Servers.AddRangeAsync(TestData.Servers);
+                await _db.SaveChangesAsync();
+            }
+            if (!await _db.Messages.AnyAsync())
+            {
+                await _db.Messages.AddRangeAsync(TestData.Messages);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
